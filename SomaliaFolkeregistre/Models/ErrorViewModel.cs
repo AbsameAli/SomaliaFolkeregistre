a211acf116eed66f1ec0c5aae2a06d783cd1d8f8
AbsameAli <55294022+AbsameAli@users.noreply.github.com>
2023-04-23 20:49:32 +0000

    MVC version

---
+++ SomaliaFolkeregistre/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SomaliaFolkeregistre.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
